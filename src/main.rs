126e24707fac2143902e6634e01739e1c8c8c756
Arindam Pal <arindampal2000@gmail.com>
2023-09-25 17:24:07 +0530

    responding with status 200 OK

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::net::TcpListener;
+use std::io::Write;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").expect("Could not bind TCP server to the port");
     
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
                 println!("accepted new connection");
+
+                let response_buf = b"HTTP/1.1 200 OK\r\n\r\n";
+                stream.write_all(response_buf).expect("Could not send a http response");
             }
             Err(e) => {
                 println!("error: {}", e);
